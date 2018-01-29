ee34e1d7202acb5b4833e01fec7d2055c758aa5b
Unknown <sodomyoindy@gmail.com>
2018-01-29 18:24:20 +0700

    เสร็จ

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,4 +1,29 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+
+    float cA(float r){
+        float area;
+        area = 3.1416*r*r;
+        return(area);}
+
 int main() {
-        /* code */
-        return 0;
-}
+    float r,cmd;
+
+
+    do{
+        cin>>cmd;
+        if (cmd==1){
+          cin>> r;
+          cout << cA(r)<<endl;
+
+        } else{break;}
+    }while(1);
+
+    return(0);
+
+
+    }
+
+
