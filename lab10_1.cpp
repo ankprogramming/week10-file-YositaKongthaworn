f891711199d4e02381e00569d42d6cbdfee1d301
Unknown <sodomyoindy@gmail.com>
2018-01-29 18:06:03 +0700

    เสร็จ

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,4 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void swap(int * a,int * b)
+   {
+    int yo;
+    yo  = * a;
+    * a = * b;
+    * b = yo;
+
+   }
+
 int main() {
-        /* code */
-        return 0;
+    int a,b,cmd;
+
+
+   cin >>a;
+   cin >>b;
+   do {
+    cout << "menu\n1.swap()\n2.quit()"<< endl;
+    cin>> cmd;
+        if (cmd==1){
+            swap(&a,&b);
+        }else {break;}
+   }
+    while(1);
+   return 0;
 }
+
+
+
+
+/*
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main() {
+ofstream myfile ("example.txt");
+if (myfile.is_open())
+{
+myfile << "this is a line.\n";
+myfile << "this is a another line.\n";
+myfile.close();
+}
+else cout << "unable to oprn file";
+return 0;
+}
+
+*/
