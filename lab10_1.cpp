727df2f042046f919773178c13f8b4e88d19b281
Unknown <sodomyoindy@gmail.com>
2018-01-30 10:56:59 +0700

    ทำใหม่

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,36 +1,3 @@
-#include <iostream>
-using namespace std;
-
-void swap(int * a,int * b)
-   {
-    int yo;
-    yo  = * a;
-    * a = * b;
-    * b = yo;
-
-   }
-
-int main() {
-    int a,b,cmd;
-
-
-   cin >>a;
-   cin >>b;
-   do {
-    cout << "menu\n1.swap()\n2.quit()"<< endl;
-    cin>> cmd;
-        if (cmd==1){
-            swap(&a,&b);
-        }else {break;}
-   }
-    while(1);
-   return 0;
-}
-
-
-
-
-/*
 #include<iostream>
 #include<fstream>
 using namespace std;
