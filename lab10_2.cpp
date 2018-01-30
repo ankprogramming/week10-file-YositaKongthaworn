7af161bc0fe0fac21599d9e99c3b9978aa01ebf9
Unknown <sodomyoindy@gmail.com>
2018-01-30 10:59:00 +0700

    เสร้จ

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,29 +1,26 @@
 #include<iostream>
 #include<fstream>
+#include<string>
 using namespace std;
 
-
-    float cA(float r){
-        float area;
-        area = 3.1416*r*r;
-        return(area);}
-
 int main() {
-    float r,cmd;
 
 
-    do{
-        cin>>cmd;
-        if (cmd==1){
-          cin>> r;
-          cout << cA(r)<<endl;
+    string line;
+    ifstream myfile ("example.txt");
 
-        } else{break;}
-    }while(1);
-
-    return(0);
-
+if (myfile.is_open())
+{
+    while (getline (myfile,line))
+    {
+        cout <<line<<'\n';
 
     }
+    myfile.close();
+}
+else cout << "unable to open file";
+return 0;
+}
+
 
 
