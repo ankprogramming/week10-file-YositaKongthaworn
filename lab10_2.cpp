91147f8f64042af253128f2f9bfbaa536b7d427c
Unknown <sodomyoindy@gmail.com>
2018-01-30 12:09:06 +0700

    เสร้จ

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -9,16 +9,25 @@ int main() {
 
 if (myfile.is_open())
 {
-    while (getline (myfile,line))
+    while (getline (myfile,line)) /*่านค่าไฟล์ เก็บไว้ใน ... ให้ผลรับเป็นจริง*/
     {
-        cout <<line<<'\n';
+        cout <<line<<'\n'; /*แล้วก้อแสดงออกมา */
 
-    }
-    myfile.close();
+    }  /*กลับขึ้นไปทำใหม่อีกรอบ*/
+    myfile.close(); /*ิดไฟล์*/
 }
 else cout << "unable to open file";
 return 0;
 }
 
+
+/*
+ประการตัวแปล
+สร้างเงื่อนไข เพื่อการอ่านไฟล์
+เราจะใช้ลูปวาย
+ใช้ฟังชันเกดไป อ่านค่าที่ล่ะ บรรทัด
+และจะให้ข้อมูลตัวนั้นไปเก็บ
+
+*/
 
 
